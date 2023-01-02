4177ee8830264cf991156e512ee76230fe7fcfc8
Aathira-M <114485876+Aathira-M@users.noreply.github.com>
2023-01-02 09:45:48 +0530

    Add files via upload

---
+++ project/javascript/index.js
@@ -0,0 +1,1 @@
+  $(".toggle_btn").click(function(){     $(this).toggleClass("active");    $(".wrapper ul").toggleClass("active");    if($(".toggle_btn").hasClass("active")){      $(".toggle_text").text("Show Less");    }    else{      $(".toggle_text").text("Show More");    }  });
